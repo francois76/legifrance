a9e016c8637cd7eda6bb946ca6b170df2a256405
francoisgognet <fgognet@gmail.com>
2023-03-06 22:22:56 +0100

    update

--- add_new.sh
+++ add_new.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 for fullNom in source/*; do
     nom=$(basename $fullNom)
-    echo $nom
     rm -rf "source/$nom"
     cd source
     git clone "https://archeo-lex.fr/codes/$nom"
@@ -10,6 +9,5 @@ for fullNom in source/*; do
     cd ../..
     git add .
     git commit -m "adding $nom"
-    exit 0
 done
 git push
