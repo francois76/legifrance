f767f6de524dba25742100a82beb8a0558c1d9a2
francoisgognet <fgognet@gmail.com>
2023-03-12 17:27:38 +0100

    refactoring code

---
+++ generate_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nom=$1
+mkdir source_md/"$nom"
+sed -E 's/(#{6})#{1,}/\1/g' source/"$nom"/"$nom".md > source_md/"$nom"/"$nom"_2.md
+sed -E 's/^#(#*) ([^:]+) :.*/#\1 \2/' source_md/"$nom"/"$nom"_2.md > source_md/"$nom"/"$nom"_1.md
+sed -E 's/^(#+\s*)(.{0,256})(.*)/$1$2.../' source_md/"$nom"/"$nom"_1.md > source_md/"$nom"/"$nom".md
+mdsplit source_md/"$nom"/"$nom".md --max-level 6 --output "$nom"-out
+rm -rf "$nom"
+mv "$nom"-out "$nom"
+find $nom -type f ! -name 'Article*' -delete
